40271eb7a307dc461b04f16a5dedd5738a48dc38
Yokoyama Ryu <yokoyamaryu1218@yahoo.co.jp>
2022-07-07 20:17:36 +0900

    出勤部分bladeデザイン変更

--- web/resources/views/top/work.blade.php
+++ web/resources/views/top/work.blade.php
@@ -1,19 +1,15 @@
 <section class="text-gray-600 body-font relative">
-    <div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
-        <div class="p-0 xs:p-0 mx-auto md:w-full md:max-w-md">
+    <div class="container px-5 py-24 mx-auto ">
+        <div class="lg:w-1/2 md:w-2/3 mx-auto">
             <div class="font-bold text-left text-2xl mb-5">07月07日（木）</div>
             <h3 class="text-right text-2xl mb-5">おはようございます、〇〇さん</h3>
             <h1 class="font-bold text-center text-2xl mb-5">16：05：00</h1>
-
-            <div class="pt-6 w-full flex space-x-2 justify-between">
-                <button class="transition duration-200 bg-green-500 hover:bg-green-600 focus:bg-green-700 focus:shadow-sm focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">出勤</button>
-                <button class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">退勤</ｂclass=>
+            <div class="flex justify-center">
+                <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">出勤</button>
+                <button class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">退勤</button>
             </div>
-
             <div class="pt-6 w-full text-right">
-                <button class="py-2.5 px-8 bg-green-400 text-white font-bold">出退勤切替</button>
+                <button class="text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg">出退勤切替</button>
             </div>
-
         </div>
-    </div>
 </section>
